96275354116bcbd5cbbb2515cc7f0f06dcba1abb
joaovsz <joaovitor1713coin@gmail.com>
2023-07-26 14:51:46 -0300

    Rota put adicionado com success

--- src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -1,17 +1,24 @@
 package com.devsuperior.userdept.controllers;
 
-import com.devsuperior.userdept.entities.Department;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.devsuperior.userdept.entities.User;
 import com.devsuperior.userdept.repositories.UserRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.HttpStatus;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users") // Mapeando o endpoint
-
 public class UserController {
     // Injeta  dependências em classes gerenciadas pelo Spring
     private final UserRepository repository;
@@ -35,20 +42,32 @@ public class UserController {
         User result = repository.save(user); // O save para inserir um usuário pelo Body da req
         return result; 
     }
-//    @PutMapping("/update/{id}")
-//    public Optional<User> editUser(@PathVariable Long id){
-//
-//        return repository.findById(id).map(user -> {
-//            user.setEmail(insert(user).getEmail());
-//            user.setName(insert(user).getName());
-//            user.setAtivo(insert(user).getAtivo());
-//            user.setDepartment(insert(user).getDepartment());
-//            return repository.save(user);
-//        }).orElseGet(()->{
-//            User insert = null;
-//            insert.setId(id);
-//        })
-//    }
+
+   @PutMapping("/update/{id}")
+   @CrossOrigin(origins="*", maxAge = 3600)
+   public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        
+        User usuarioExistente = repository.findById(id).get();
+         if (usuarioExistente == null) {
+             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+         }
+
+        if (updatedUser.getName() != null) {
+            usuarioExistente.setName(updatedUser.getName());
+        }
+
+        if (updatedUser.getEmail() != null) {
+            usuarioExistente.setEmail(updatedUser.getEmail());
+        }
+
+        if (updatedUser.getDepartment() != null) {
+            usuarioExistente.setDepartment(updatedUser.getDepartment());
+        }
+
+       repository.save(usuarioExistente);
+
+       return new ResponseEntity<>(usuarioExistente, HttpStatus.OK);
+    }
     @DeleteMapping(path="/delete/{id}")
     @CrossOrigin(origins="*", maxAge = 3600)
     public ResponseEntity<String> delete(@PathVariable Long id){
