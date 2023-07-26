b1ad44c40b41c1fc31a07ca48204372fed8c2e01
joaovsz <joaovitor1713coin@gmail.com>
2023-07-26 14:06:03 -0300

    Add Delete endPoint

--- src/main/java/com/devsuperior/userdept/controllers/UserController.java
+++ src/main/java/com/devsuperior/userdept/controllers/UserController.java
@@ -1,22 +1,24 @@
 package com.devsuperior.userdept.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devsuperior.userdept.entities.Department;
 import com.devsuperior.userdept.entities.User;
 import com.devsuperior.userdept.repositories.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users") // Mapeando o endpoint
+
 public class UserController {
-    @Autowired // Injeta  dependências em classes gerenciadas pelo Spring
-    private UserRepository repository;
+    // Injeta  dependências em classes gerenciadas pelo Spring
+    private final UserRepository repository;
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping // Indica que é uma requisição HTTP
     public List<User> findAll() { // Retorna uma lista de usuários do banco de dados
         List<User> result = repository.findAll();
@@ -27,10 +29,37 @@ public class UserController {
         User result = repository.findById(id).get(); // O get para pegar o usuário pelo id
         return result;
     }
-    
+
     @PostMapping // Indica que é uma requisição HTTP para inserção
     public User insert(@RequestBody User user) { 
         User result = repository.save(user); // O save para inserir um usuário pelo Body da req
         return result; 
     }
+//    @PutMapping("/update/{id}")
+//    public Optional<User> editUser(@PathVariable Long id){
+//
+//        return repository.findById(id).map(user -> {
+//            user.setEmail(insert(user).getEmail());
+//            user.setName(insert(user).getName());
+//            user.setAtivo(insert(user).getAtivo());
+//            user.setDepartment(insert(user).getDepartment());
+//            return repository.save(user);
+//        }).orElseGet(()->{
+//            User insert = null;
+//            insert.setId(id);
+//        })
+//    }
+    @DeleteMapping(path="/delete/{id}")
+    @CrossOrigin(origins="*", maxAge = 3600)
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        User userToDelete = repository.findById(id).orElse(null);
+
+        if (userToDelete!=null){
+            repository.delete(userToDelete);
+            return ResponseEntity.ok("Usuario excluido com sucesso");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario nao encontrado");
+        }
+    }
+
 }
